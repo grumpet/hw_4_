29a60d85d518cc2798e1f6b657da472555889bec
grumpet <131244030+grumpet@users.noreply.github.com>
2023-05-28 18:24:12 +0300

    Add files via upload

---
+++ InterestBearing.java
@@ -0,0 +1,9 @@
+package hw_4;
+
+public interface InterestBearing {
+	public double computeIntres(int years);
+	public final static double INTEREST_RATE = 0.05;
+	
+	
+
+}
---
+++ SavingAccount.java
@@ -0,0 +1,5 @@
+package hw_4;
+
+public class SavingAccount {
+
+}
